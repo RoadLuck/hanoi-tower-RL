4892649524e5816a80ca146b1c630ef7a6e29bb9
roadluck <vcontreras@talana.com>
2021-07-12 21:09:26 -0400

    Modificaciones

--- agent.py
+++ agent.py
@@ -1,39 +1,34 @@
 import numpy as np
 
 class Agent():
-    def __init__(self, lr, gamma, n_actions, n_states, epsilon):
+    def __init__(self, lr, gamma, n_actions, n_states, eps_start, eps_end,
+                 eps_dec):
+        
         self.lr = lr
         self.gamma = gamma
         self.n_actions = n_actions
         self.n_states = n_states
-        self.epsilon = epsilon
+        self.epsilon = eps_start
+        self.eps_min = eps_end
+        self.eps_dec = eps_dec
 
+        self.Q = np.zeros((self.n_states, self.n_actions), dtype=float)
 
-        self.Q = {}
-
-        #self.init_Q()
-
-    def init_Q(self):
-        for state in range(self.n_states):
-            for action in range(self.n_actions):
-                print((state,action))
-                self.Q[state, action] = 0.0
-        print(self.Q)
 
     def choose_action(self, state):
         if np.random.random() < self.epsilon:
             action = np.random.choice([i for i in range(self.n_actions)])
         else:
-            action = np.random.choice([i for i in range(self.n_actions)])
-            actions = np.array([self.Q[state, a] for a in range(self.n_actions)])
+            actions = np.array([self.Q[(state, a)] for a in range(self.n_actions)])
+            print(actions)
             action = np.argmax(actions)
         return action
 
-    def learn(self, state, action, reward, state_):
-        #print("S",state)
-        #print("A1",action)
-        #print("S'",state_)
-        actions = np.array([self.Q[state_, a] for a in range(self.n_actions)])
-        a_max = np.argmax(actions)
+    def decrement_epsilon(self):
+        self.epsilon = self.epsilon*self.eps_dec if self.epsilon>self.eps_min\
+                       else self.eps_min
 
-        self.Q[state, action] += self.lr*(reward + self.gamma*self.Q[state_, a_max] - self.Q[state, action])
+    def learn(self, state, action, reward, state_):
+        #print(self.Q)
+        self.Q[(state, action)] += self.lr*(reward+self.gamma*np.max(self.Q[state_,:])-self.Q[(state,action)])
+        self.decrement_epsilon()
--- hanoi-q-learn.py
+++ hanoi-q-learn.py
@@ -7,37 +7,37 @@ from agent import Agent
 if __name__ == '__main__':
     env = gym.make('Hanoi-v0')
     env.num_disk = 3
+
+    actions = env.action_space.n
+    states = len(env.observation_space)*3
+
+    print(actions)
     
-    agent = Agent(lr=0.001, gamma=0.9, epsilon=0.9, n_actions=6, n_states=8)
+    agent = Agent(lr=0.85, gamma=0.95, n_actions=actions, n_states=states, eps_start=0.9, eps_end=0.1, eps_dec=0.999)
 
     scores = []
     win_pct_list = []
-    n_games = 1
-    #print(env.num_disks)
-    observation = env.reset()
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
-    print(action)
-    print(obs)
-    print(reward)
+    n_games = 10
 
     for i in range(n_games):
         done = False
         observation = env.reset()
         score = 0
+        print("Holaa")
         
         while not done:
             action = agent.choose_action(observation)
+            print(action)
             observation_, reward, done, info = env.step(action)
             agent.learn(observation, action, reward, observation_)
             score += reward
             observation = observation_
-        scores.append(score)
-        if i % 100 == 0:
-            win_pct = np.mean(scores[-100:])
-            win_pct_list.append(win_pct)
-            if i % 1000 == 0:
-                print('episode ', i, 'win pct %.2f' % win_pct,
-                      'epsilon %.2f' % agent.epsilon)
+            scores.append(score)
+    if i % 100 == 0:
+        win_pct = np.mean(scores[-100:])
+        win_pct_list.append(win_pct)
+    if i % 1000 == 0:
+            print('episode ', i, 'win pct %.2f' % win_pct,
+                'epsilon %.2f' % agent.epsilon)
     plt.plot(win_pct_list)
     plt.show()
